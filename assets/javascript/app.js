173715f9491a5992147b567f4be3dda9dd9014ed
myusername <rodriguez.jamie@live.com>
2018-09-12 21:01:40 -0400

    clock is counting down, questions pushed to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,37 @@
 // alert("something");
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
 
 // Creating variables to hold the questions and answers in an object
-var questions = {question1: false, question2: true};
+var questions = [
+    {
+      question: "The sky is blue.",
+      options: { a: true, b: false },
+      answer: true
+    },
+    {
+      question: "There are 365 days in a year.",
+      options: { a: true, b: false },
+      answer: true
+    },
+    {
+      question: "There are 42 ounces in a pound.",
+      options: { a: true, b: false },
+      answer: false
+    },
+    {
+      question: "The Declaration of Independence was created in 1745.",
+      options: { a: true, b: false },
+      answer: false
+    },
+    {
+      question: "Bananas are vegetables.",
+      options: { a: true, b: false },
+      answer: false
+    }
+  ];
+
+var questionList=0;
 
 //Created variable for wrong, correct and unaswered questions
 var wrongAnswer = 0;
@@ -11,25 +41,43 @@ var unanswered = 0;
 //create var for timer
 var timer = 0;
 //set interval that runs the stopwatch
-var intervalId;
+var intervalId = 0;
 
-// function countdown (params) {
-//     oneminute = 0;
-//     onesecond = 0;
-//     timer = setTimeout(oneminute, 1000 * 60) ;  
-//     //set the "timer" div to "00:60."
-//     $("#timer").text("00:60");
-//     //set interval that runs the stopwatch
-//     intervalId = setInterval(onesecond, 1000);
-// }
-//create function to start game and include timer
+  
+function showQuestions(params) {
+    for (var i = 0; i < questions.length; i++) {
+    questionsList = questions[i]
+    console.log("This is: " + questionsList);
+    // Then prepend the questions to the top of our question div.
+    $("#quest").append("<br><hr>" + questionsList.question + "<br>" +
+      //create radio buttons for all the options.
+      questionsList.options.a +
+      '<label><input type="radio" name=:"' + questionsList.question.length + '" value="' + questionsList.options.a + '" /></label>' + "<br>" +
+      questionsList.options.b +
+      '<label><input type="radio" name=:"' + questionsList.question.length + '" value="' + questionsList.options.b + '" /></label>' + "<br>" + "Answer = " +
+      questionsList.answer);
+  }
+}
+
+
+//if statement loop through and check if selected radio = to answer
+
+// check if no answer was provided, push to no answer variable and add to each no answer
+// send all answers to array []
+// check if answer given === the answer in object. and push ++ the count of right answers up.
+
+// }//create function to start game and include timer
 // function start (params) {
     //when start button is clicked;
     $("#startBtn").on("click", function () {
         //start timer
-        start();
+        startTimer ();
         //show questions
-    
+        showQuestions();
+        //if timer runs down stop timer
+        if (timer === 0) {
+        stop();
+        }
     });
 
 
@@ -41,35 +89,30 @@ var intervalId;
 
 //append answers in the page reveals number of questions that were answered correctly
 
-//Dont let player pick more than one answer
-//Create radio to only choose one option.
-//
 // Solution if you choose not to put it in an object
-
-
-
-  
+ 
 // prevents the clock from being sped up unnecessarily
 var clockRunning = false;
 
-function start() {
+function startTimer () {
     timer = 60;
     $("#timer").text("00:60");
         if (!clockRunning){
         intervalId = setInterval(count, 1000);
         clockRunning = true;
-        }
+        }    
+        //set timer to stopp when it reaches 0
+        // else {
+        // (timer === 0)
+        // stop();
+        // }
 }
 
-    //set timer to stopp when it reaches 0
-        // if (timeConverter(timer) == "00"){
-        //     stop();
-        // }
 
-
+//set timer to stop
 function stop() {
-    console.log("stopping");
-    clearInterval(intervalId);
+  clearInterval(intervalId);
+  console.log("stopping");
 }
 
 function count() {
@@ -78,36 +121,21 @@ function count() {
   $("#timer").text(converted);
 }
 
+//Math function coverts timer 00:00
 function timeConverter(t) {
-
   var minutes = Math.floor(t / 60);
   var seconds = t - (minutes * 60);
 
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
-
   if (minutes === 0) {
     minutes = "00";
   }
   else if (minutes < 10) {
     minutes = "0" + minutes;
   }
-
   return minutes + ":" + seconds;
 }
 
-
-//     //function to run else if statments if equal, or over
-//     function elsify (params) {
-//             if (gemTotal === randomNumber) {
-//                 console.log("You Win");
-//                 wins++;
-//                 reset()
-//             }
-//             else if (gemTotal > randomNumber){
-//                 console.log("You went over the random number. You Lost.");
-//                 losses++;
-//                 reset()
-//             }
-//    
+});
