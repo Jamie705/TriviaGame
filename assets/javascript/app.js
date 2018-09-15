b1946cb919dadf12b0d15bb7b9d16689ea7b1194
myusername <rodriguez.jamie@live.com>
2018-09-14 22:05:53 -0400

    Radio buttons are not returning value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,44 @@
 // alert("something");
+// $(document).ready(function () {
 // document.addEventListener("DOMContentLoaded", function (event) {
 //   console.log("DOM fully loaded and parsed");
 
 // Creating variables to hold the questions and answers in an object
 var questions = [
     {
-      question: "The sky is blue.",
+      question: "The NFL football feild is 160 feet wide",
       options: { a: true, b: false },
       answer: true
     },
     {
-      question: "There are 365 days in a year.",
+      question: "The longest field goal made in the NFL is 64 yards.",
       options: { a: true, b: false },
       answer: true
     },
     {
-      question: "There are 42 ounces in a pound.",
+      question: "The New Englad Patriots have 8 superbowl rings.",
       options: { a: true, b: false },
       answer: false
     },
     {
-      question: "The Declaration of Independence was created in 1745.",
+      question: "The NFL football used is made of 'pig-skin'.",
       options: { a: true, b: false },
       answer: false
     },
     {
-      question: "Bananas are vegetables.",
+      question: "The San Fransisco 49ers scored the most points in a superbowl.",
       options: { a: true, b: false },
-      answer: false
+      answer: true
     }
   ];
 
 var questionList=0;
 
 //Created variable for wrong, correct and unaswered questions
-var wrongAnswer = 0;
-var correctAnswer = 0;
-var unanswered = 0;
+var wrongAnswers = 0;
+var correctAnswers = 0;
 
+var arrAnswers = questions[0].answer;
 //create var for timer
 var timer = 0;
 //set interval that runs the stopwatch
@@ -45,31 +46,63 @@ var intervalId = 0;
 var radioAnswers = []
   
 function showQuestions(params) {
-    $("#submit").append(radioAnswers.push);
-    
+  
+    //for loop to issue questions & radio buttons   
     for (var i = 0; i < questions.length; i++) {
     questionsList = questions[i];
-    console.log("This is the questionlist: " + questions[i]);
+    console.log("This is the question list: " + questionsList.question);
     
     // Then prepend the questions to the top of our question div.
-    $("#quest").append("<br><hr>" + questionsList.question + "<br>" +
+    $("#quest").prepend("<br><hr>" + questionsList.question + "<br>" +
         
     //create radio buttons for all the options.
       // questionsList.options.a +
-      '<label>True<input type="radio" name=:"' + questionsList.question.length + '" value="' + questionsList.options.a + '" /></label>' + "<br>" +
+      '<form><label>True<input type="radio" name=:"' + questionsList.question + '" value="' + questionsList.options.a + '" /></label>' + "<br>" +
       // questionsList.options.b +
-      '<label>False<input type="radio" name=:"' + questionsList.question.length + '" value="' + questionsList.options.b + '" /></label>' + "<br>" +
+      '<label>False<input type="radio" name=:"' + questionsList.question + '" value="' + questionsList.options.b + '" /></label></form>' + "<br>"
       //login answers
-      "Answer = " + questionsList.answer);
-  }
-}
+      // "Answer = " + questionsList.answer
+      );
+    }
+   
+         //submit button to generate answers and add them to array or compare to correct answers
+        $("#quest").append('<button id=:"submitBtn">Submit</button>'); 
+        
+        $("button").click(function () {
+            $("input[name='questionsList.question']:checked")        
+            radioAnswers.push($("input").val());
+            console.log(radioAnswers.length + radioAnswers);
+          // check if answer given === the answer in object. and push ++ the count of right answers up.
+            //will use function elsify
+            //append answers in the page reveals number of questions that were answered correctly
+            $("#score").css("display", "block");
+        });
+} 
 
+//total array answers minus- correctAnswers = Wrong answer
+wrongAnswers = arrAnswers.length - correctAnswers.length;
 
-//if statement loop through and check if selected radio = to answer
+//function to insert else if statments
+  function elsify (params) {
+    //length of answers for both guessed correclty and answer array means all answers were correct
+    if (radioAnswer.length === arrAnswers.length) {
+      console.log("You got them all right")
+      //should equal same amount of questions right
+      correctAnswers = question.length;
+      //update score
+      $('#correctAnswer').text("Correct Answers: " + correctAnswers);
+      } 
+      //compare how many radio answers were correct and update
+    else if (radioAnswer.length != arrAnswers.length){
+      //show correct answers
+      console.log("Answered Correctly: " + correctAnswers.length)
+      console.log("Inorrectly Answered: " + wrongAnswers)
+      $('#correctAnswer').text("Correct Answers: " + correctAnswers.length);
+      $('#wrongAnswer').text("Wrong Answers: " + wrongAnswers);
+    }
+  };            
 
 // check if no answer was provided, push to no answer variable and add to each no answer
-// send all answers to array []
-// check if answer given === the answer in object. and push ++ the count of right answers up.
 
 // }//create function to start game and include timer
 // function start (params) {
@@ -79,10 +112,6 @@ function showQuestions(params) {
         startTimer ();
         //show questions
         showQuestions();
-        //if timer runs down stop timer
-        if (timer === 0) {
-        stop();
-        }
     });
 
 
@@ -94,7 +123,6 @@ function showQuestions(params) {
 
 //append answers in the page reveals number of questions that were answered correctly
 
-// Solution if you choose not to put it in an object
  
 // prevents the clock from being sped up unnecessarily
 var clockRunning = false;
@@ -105,7 +133,6 @@ function startTimer () {
         if (!clockRunning){
         intervalId = setInterval(count, 1000);
         clockRunning = true;
-      
         }    
 }
 
