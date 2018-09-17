c5dd2349594b8233bd4cd76cb83b0c89de1e25a3
myusername <rodriguez.jamie@live.com>
2018-09-16 23:42:56 -0400

    Redid questions into html, now all working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,143 +1,181 @@
 // alert("something");
 // $(document).ready(function () {
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   console.log("DOM fully loaded and parsed");
 
 // Creating variables to hold the questions and answers in an object
-var questions = [
+var footballList = [
     {
       question: "The NFL football feild is 160 feet wide",
       options: { a: true, b: false },
-      answer: true
+      answer: "true"
     },
     {
       question: "The longest field goal made in the NFL is 64 yards.",
       options: { a: true, b: false },
-      answer: true
+      answer: "true"
     },
     {
       question: "The New Englad Patriots have 8 superbowl rings.",
       options: { a: true, b: false },
-      answer: false
+      answer: "false"
     },
     {
       question: "The NFL football used is made of 'pig-skin'.",
       options: { a: true, b: false },
-      answer: false
+      answer: "false"
     },
     {
       question: "The San Fransisco 49ers scored the most points in a superbowl.",
       options: { a: true, b: false },
-      answer: true
+      answer: "true"
     }
   ];
 
   //answers in array
 var arrAnswers =[];
   //created for loop to put answers in array
-for (let j = 0; j < questions.length; j++) {
-  arrAnswers.push(questions[j].answer);
-    console.log(arrAnswers);
+for (let j = 0; j < footballList.length; j++) {
+  arrAnswers.push(footballList[j].answer);
+  // console.log(arrAnswers);
 }
-
-var questionList=0;
 
 //Created variable for wrong, correct and unaswered questions
 var wrongAnswers = 0;
 var correctAnswers = 0;
-
 
 //create var for timer
 var timer = 0;
 //set interval that runs the stopwatch
 var intervalId = 0;
 var radioAnswers = [];
-  
-function showQuestions(params) {
-  
-    //for loop to issue questions & radio buttons   
-    for (var i = 0; i < questions.length; i++) {
-    questionsList = questions[i];
-    console.log("This is the question list: " + questionsList.question);
-    
-    // Then prepend the questions to the top of our question div.
-      
-    $("#quest").prepend("<br><hr>" + questionsList.question + "<br>" +
-     
-    //create radio buttons for all the options.
-      // Radio button for true
-      '<form><label>True<input type="radio" name="' + questionsList.question + '" value="' + true + '" /></label>' + "<br>" +
-      // Radio button for false
-      '<label>False<input type="radio" name="' + questionsList.question + '" value="' + false + '" /></label></form>' + "<br>"
-      //login answers
-      // "Answer = " + questionsList.answer
-      );
-    }
-    // event.preventDefault();
-        //submit button to generate answers and add them to array or compare to correct answers
-    $("#quest").append('<button id="submitBtn">Submit</button>'); 
-    //create click event, on button, only displays 1 answer on all it displays
-    $("input").click(function () {
-      $('input:radio [name="'+ questionsList.question + '"]:checked');        
-      radioAnswers.push($("input").val());
-      console.log(radioAnswers.length + radioAnswers);
-      // check if answer given === the answer in object.
-      elsify();
-      //append answers in the page reveals number of questions that were answered correctly
-      $("#score").css("display", "block");
-    });
-} 
 
-//total array answers minus- correctAnswers = Wrong answer
-wrongAnswers = 0; //arrAnswers.length - correctAnswers.length;
-correctAnswers = 0; //TODO: radioAnswers compared to array answers create function to compare answers
-
-// $('#checkAns').click(function (e) {
-//   $.each(answers, function (question, answer) {
-//     if ($('input:radio[name="' + question + '"]:checked').val() == answer)
-//       alert(question + ': True');
-//     else
-//       alert(question + ': False!');
-//   });
-//   e.preventDefault();
-// });
-
-//function to insert else if statments
-  function elsify (params) {
-    //length of answers for both guessed correclty and answer array means all answers were correct
-    if (radioAnswers === arrAnswers) {
-      console.log("You got them all right");    
-      //update score
-      $('#correctAnswer').text("Correct Answers: " + correctAnswers);
-    }
-
-      //if else statment when game is finished  time runs out.
-      //TODO: compare how many radio answers were correct and update
-      // all answers generate and they are not all correct
-    else if (radioAnswers != arrAnswers ){
-      //show all answers correct and wrong
-      console.log("Answered Correctly: " + correctAnswers.length);
-      console.log("Inorrectly Answered: " + wrongAnswers);
-      $('#correctAnswer').text("Correct Answers: " + correctAnswers.length);
-      $('#wrongAnswer').text("Wrong Answers: " + wrongAnswers);
-    }
-  }            
-
-// function start (params) {
+  // function start (params) {
     //when start button is clicked;
     $("#startBtn").on("click", function () {
-        //start timer
-        startTimer ();
-        //show questions
-        showQuestions();
+      //start timer
+      startTimer ();
+      //show questions
+      startDisplay();
     });
 
+    //funtion to show/hide start menu
+function startDisplay(params) {
+  $("#quest").css("display", "initial");
+  $("#instructions").css("display", "none");
+  $("#startBtn").css("display", "none");
+  $("#restartBtn").css("display", "none");
+}
 
-//Game ends when timer runs out or done button is clicked
-//Create done/finish button 
+function finishDisplay(params) {
+  $("#score").css("display", "block");
+  $("#quest").css("display", "none");
+  // initialize restart
+  $("#restartBtn").css("display", "initial");
+  restart();
+}
+//function to update score
+function update (params) {
+  $("#correct").text(correctAnswers);
+  $("#wrong").text(wrongAnswers);
+}
 
+//restart game/ change or create new button to restart
+function restart(params) {
+//restart with new button, on click resets all
+  $("#restartBtn").on("click", function () {
+    //start timer
+    startTimer();
+    //show questions
+    startDisplay();
+    //zero variables and score
+    wrongAnswers = 0;
+    correctAnswers = 0;
+    radioAnswers = [];
+    update()
 
+  });
+}
+// function getAnswers(param)
+//*****************Get Answers***************************** */
+    //create click event, on button, only displays 1 answer on all it displays
+    $("#results").click(function () {
+      //show scoreboard
+      finishDisplay();
+
+       //for loop to issue questions & radio buttons did not work
+      $('input[name= "q1"]:checked');
+      radioAnswers.push($('input[name= "q1"]:checked').val());
+      console.log("q1: " + radioAnswers);
+      //if else
+      if (radioAnswers[0] === arrAnswers[0]) {
+        correctAnswers++;
+      }
+      else {
+        (radioAnswers[0] != arrAnswers[0]);
+        wrongAnswers++;
+      }
+      update();
+       //question 2
+      $('input[name= "q2"]:checked').val();
+      radioAnswers.push($('input[name= "q2"]:checked').val());
+      console.log("q2: " + radioAnswers);
+      
+      if (radioAnswers[1] === arrAnswers[1]) {
+        correctAnswers++;
+      }
+      else {
+        (radioAnswers[1] != arrAnswers[1]);
+        wrongAnswers++;
+      }
+      update();
+      //question 3
+      $('input[name= "q3"]:checked').val();
+      radioAnswers.push($('input[name= "q3"]:checked').val());
+      console.log("q3: " + radioAnswers);
+      if (radioAnswers[2] === arrAnswers[2]) {
+        correctAnswers++;
+      }
+      else {
+        (radioAnswers[2] != arrAnswers[2]);
+        wrongAnswers++;
+      }
+      update();
+
+      //question 4
+      $('input[name= "q4"]:checked').val();
+      radioAnswers.push($('input[name= "q4"]:checked').val());
+      console.log("q4" + radioAnswers);
+      
+      if (radioAnswers[3] === arrAnswers[3]) {
+        correctAnswers++;
+      }
+      else {
+        (radioAnswers[3] != arrAnswers[3]);
+        wrongAnswers++;
+      }
+      update();
+      //question 5
+      $('input[name= "q5"]:checked').val();
+      radioAnswers.push($('input[name= "q5"]:checked').val());
+      console.log("q5" + radioAnswers);
+    
+      if (radioAnswers[4] === arrAnswers[4]) {
+        correctAnswers++;
+      }
+      else {
+        (radioAnswers[4] != arrAnswers[4]);
+        wrongAnswers++;
+      }
+      update();
+      //turn off timer
+        clearInterval(intervalId);
+        console.log("Timer Off");
+        clockRunning = false;
+        $("#timer").text("00");
+
+    });
+   
 // prevents the clock from being sped up unnecessarily
+//***************TIMER***************
 var clockRunning = false;
 
 function startTimer () {
@@ -157,6 +195,8 @@ function count() {
     clearInterval(intervalId);
     console.log("Time is up");
     clockRunning = false;
+    $("#score").css("display", "block");
+
   }
 }
 
